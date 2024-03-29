97551a04901384901c8b0264c291a33d78603f07
abhinavbgpgit <abhinavbgp@gmail.com>
2024-03-29 06:48:00 +0530

    changed color

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,7 +3,7 @@ import manipal_logo from "../../assets/images/LOGO-1.png";
 
 const Login = () => {
   return (
-    <div className="bg-gradient-to-r from-blue-700 to-blue-500 h-screen flex  items-center justify-center">
+    <div className="bg-gradient-to-r from-gray-300 to-gray-300 h-screen flex  items-center justify-center">
       <div className="w-[60%] h-5/6 justify-start items-center rounded-2xl flex flex-col bg-login-image bg-cover relative shadow-basic">
         <img className="  w-60 ml-6 mt-4" src={manipal_logo} alt="" />{" "}
         <LoginBlock />
