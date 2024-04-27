bc0149fb6cab973bd4ce71a90bb59756c3274dea
Kislay Raj <kislayraj751@gmail.com>
2024-04-27 09:18:38 +0530

    first UI update

--- src/pages/patient/Patient.tsx
+++ src/pages/patient/Patient.tsx
@@ -6,6 +6,7 @@ const Patient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -52,40 +53,61 @@ const Patient = () => {
   }
 
   return (
-    <div className="patient-list px-4 py-2">
-      <div className="patient-header flex border-b border-gray-200 justify-between items-center px-2 py-2 font-medium">
-        <div className="w-1/4">Patient Name</div>
-        <div className="w-1/6">Gender</div>
-        <div className="w-1/6">Age</div>
-        <div className="w-1/6">Contact</div>
-        <div className="w-1/6 hidden sm:block">Email</div>
-        <div className="w-1/6 text-center">Actions</div>
-        {/* Add more headers as needed */}
-      </div>
-      {patients.map((patient) => (
-        <div key={patient._id} className="patient-row flex border-b border-gray-200 justify-between items-center px-2 py-2">
-          <div className="w-1/4">{patient.patientName}</div>
-          <div className="w-1/6">{patient.gender}</div>
-          <div className="w-1/6">{patient.age}</div>
-          <div className="w-1/6">{patient.contact}</div>
-          <div className="w-1/6 hidden sm:block">{patient.email}</div>
-          <div className="w-1/6 flex justify-center space-x-2">
-            <button
-              className="px-2 py-1 text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 rounded-md"
-              onClick={() => handleEdit(patient._id)}
-            >
-              Edit
-            </button>
-            <button
-              className="px-2 py-1 text-red-500 hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 rounded-md"
-              onClick={() => handleDelete(patient._id)}
-            >
-              Delete
-            </button>
-          </div>
-          {/* Add more values as needed */}
+    <div className="patient-list px-4 py-2 ">
+      <div className=' mt-4 p-4 rounded-md bg-white'>
+        <div className='p-2 font-bold text-2xl'>Patient List</div>
+        <div className="patient-header flex border-b border-gray-200 justify-between items-center px-2 py-2 font-medium">
+          <div className="w-1/4 ">Patient Name <i className={`fa-solid fa-arrow-up text-xs text-gray-300 `}></i> <i className={`fa-solid fa-arrow-down  text-xs text-gray-300`}></i></div>
+          <div className="w-1/6">Gender <i className={`fa-solid fa-arrow-up text-xs text-gray-300 `}></i> <i className={`fa-solid fa-arrow-down  text-xs text-gray-300`}></i></div>
+          <div className="w-1/6">Age <i className={`fa-solid fa-arrow-up text-xs text-gray-300 `}></i> <i className={`fa-solid fa-arrow-down  text-xs text-gray-300`}></i></div>
+          <div className="w-1/6 ">Contact</div>
+          <div className="w-1/6 hidden sm:block">Email</div>
+          <div className="w-1/6 text-center">Actions</div>
+          {/* Add more headers as needed */}
         </div>
-      ))}
+        <div>
+          {patients.map((patient) => (
+            <div key={patient._id} className=" font-medium patient-row flex border-b border-gray-100  justify-between items-center px-2 py-2 hover:scale-[1.001] hover:bg-gray-100 transition-all duration-300">
+              {/* change accordingly */}
+              <div className="w-1/4 ">{patient.patientName[0].toUpperCase() + patient.patientName.slice(1)}</div> 
+              <div className="w-1/6">{patient.gender[0].toUpperCase() + patient.gender.slice(1)}</div>
+              <div className="w-1/6">{patient.age}</div>
+              <div className="w-1/6 text-blue-500">{patient.contact}</div>
+              <div className="w-1/6 hidden sm:block">{patient.email}</div>
+              <div className="w-1/6 flex justify-center space-x-2">
+                <button
+                  className="px-2 py-1 text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 rounded-md"
+                  onClick={() => handleEdit(patient._id)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="px-2 py-1 text-red-500 hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 rounded-md"
+                  onClick={() => handleDelete(patient._id)}
+                >
+                  Delete
+                </button>
+              </div>
+              {/* Add more values as needed */}
+            </div>
+          ))}
+        </div>
+        <div className='flex justify-between pr-8 py-2 mt-2'>
+       
+        <div className='bg-red-500 text-white w-fit p-2 rounded-md'>Showing 1 to {patients.length}</div>
+        {/* <div>pagination</div> */}
+      
+          <div className='flex gap-2 bg-gray-200 rounded-md'>
+            <button className='px-2 py-1 text-gray-500 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 rounded-md'>Previous</button>
+            <button className='px-2 py-1 w-12 text-white bg-red-500 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 focus:rounded-md'>1</button>
+            <button className='px-2 py-1 text-gray-500 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 rounded-md'>Next</button>
+
+        </div>
+      
+      </div>
+        
+      </div>
+
     </div>
   );
 };
