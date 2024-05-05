e9ada5181926819670bef85061409b8af06f184e
Avinash8294 <avinash8521499747@gmail.com>
2024-05-06 01:37:15 +0530

    leftbar changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx,js}',
   ],
   prefix: "",
   theme: {
