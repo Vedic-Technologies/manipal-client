9279e5ac2890bfe64a8d6156e09657931eeb4ebe
abhinavbgpgit <abhinavbgp@gmail.com>
2024-05-18 03:39:57 +0530

    j

--- src/pages/login/LoginBlockNew.tsx
+++ src/pages/login/LoginBlockNew.tsx
@@ -11,13 +11,13 @@ const LoginBlockNew = () => {
               </div>
               <div className="w-1/2 p-5 ">
                 <div className="text-center">
-                  <h2 className=" text-zinc-900 text-4xl font-bold mt-10" style={{fontFamily:"Edu VIC WA NT Beginner"}}>Sign In</h2>
+                  <h2 className=" text-zinc-900 text-5xl font-bold mt-10" style={{fontFamily:"Edu VIC WA NT Beginner"}}>Sign In</h2>
                 </div>
                 <div className=" center">
                 <form className="space-y-6 mt-20  w-[70%]">
                   <div>
                     <label htmlFor="email" className="text-sm font-medium text-zinc-700 block mb-2">Email</label>
-                    <input type="email" name="email" id="email" placeholder="demo@example.com" className="w-full px-3 py-2 border border-zinc-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" required />
+                    <input type="email" name="email" id="email" placeholder="Enter your email" className="w-full px-3 py-2 border border-zinc-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" required />
                   </div>
                   <div>
                     <label htmlFor="password" className="text-sm font-medium text-zinc-700 block mb-2">Password</label>
