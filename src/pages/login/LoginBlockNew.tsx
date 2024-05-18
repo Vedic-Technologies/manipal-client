4dc5989bdc8a111f499cf6f436c5fe835d850d42
abhinavbgpgit <abhinavbgp@gmail.com>
2024-05-18 06:50:38 +0530

    asd

--- src/pages/login/LoginBlockNew.tsx
+++ src/pages/login/LoginBlockNew.tsx
@@ -14,7 +14,7 @@ const LoginBlockNew = () => {
                   <h2 className=" text-zinc-900 text-5xl font-bold mt-10" style={{fontFamily:"Edu VIC WA NT Beginner"}}>Sign In</h2>
                 </div>
                 <div className=" center">
-                <form className="space-y-6 mt-20  w-[70%]">
+                <form className="space-y-6 mt-14  w-[70%]">
                   <div>
                     <label htmlFor="email" className="text-sm font-medium text-zinc-700 block mb-2">Email</label>
                     <input type="email" name="email" id="email" placeholder="Enter your email" className="w-full px-3 py-2 border border-zinc-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" required />
