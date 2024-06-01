1db78ed56e942c81ccce8823d129eae9b2eb39b2
Avinash8294 <avinash8521499747@gmail.com>
2024-06-02 00:51:24 +0530

    login with credentials

--- src/custom_components/StartupApiCall.tsx
+++ src/custom_components/StartupApiCall.tsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-export async function startApi()
-{
-    try {
-        const response = await axios.get('https://manipal-server.onrender.com/api/patient/all_patients');
-       console.log("start up data : ", response.data)
-      } catch (error) {
-       console.log("data has not come yet. Please wait for 50 seconds")
-      } finally {
-       console.log("")
-      }
-    };
+export async function startApi() {
+  try {
+    const response = await axios.get(
+      "https://manipal-server.onrender.com/api/patient/all_patients",
+      { withCredentials: true }
+    );
+    console.log("start up data : ", response.data);
+  } catch (error) {
+    console.log("data has not come yet. Please wait for 50 seconds");
+  } finally {
+    console.log("");
+  }
+}
